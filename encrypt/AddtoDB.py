2a9a9b8f44b1cdec60e4d08410eb05e71c77227c
arpith.pathange <arpith.pathange@gmail.com>
2017-09-06 14:51:36 +0530

    Some Code refactoring

--- encrypt/AddtoDB.py
+++ encrypt/AddtoDB.py
@@ -1,20 +1,29 @@
 from prettytable import PrettyTable
 from dbconn import Sqlite
 from encryptdata import SafeData
+from logger import logbase
+import sys
 
 
 class AddtoDb:
     obj_db = None
     safedata = None
     obj_AddtoDb = None
+    log = None
 
     def __init__(self):
         self.obj_db = Sqlite()
         self.safedata = SafeData()
+        self.log = logbase()
 
     def addtoDb(self,acc_info, user_info, user_pass, account_no, pin):
-        self.obj_db.dbinsert(acc_info, user_info, self.safedata.encrypt(user_pass),
+        try:
+            self.obj_db.dbinsert(acc_info, user_info, self.safedata.encrypt(user_pass),
                              account_no, self.safedata.encrypt(pin))
+            self.log.loginfo("inserted data in to the Db "+ acc_info +" "+user_info+" "+" "+
+                        self.safedata.encrypt(user_pass)+" "+account_no+" "+self.safedata.encrypt(pin))
+        except :
+            self.log.logerror("Error in inserting data to the DB "+sys.exc_info()[0])
 
     def getdata(self, pass_key):
         datas = self.obj_db.getdata()
