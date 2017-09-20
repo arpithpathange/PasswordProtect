6325ec806e4bb8a072e1b6972e9932cc9181bca8
arpith.pathange <arpith.pathange@gmail.com>
2017-09-20 12:14:46 +0530

    Shell script for auto setup of the project

--- encrypt/init.py
+++ encrypt/init.py
@@ -1,10 +1,18 @@
 __author__ = 'arpith.pathange'
 
 import sqlite3
+from encryptdata import SafeData
 
-conn = sqlite3.connect(r"account.sqlite")
+safedata = SafeData()
+
+conn = sqlite3.connect("account2.sqlite")
 c = conn.cursor()
-c.execute('''CREATE TABLE `Account` ( `account_name` TEXT,
+c.execute('''CREATE TABLE `Account` ( 'account_name' TEXT,
                         `User_name` TEXT, `Password` TEXT, `account_no`
-                            TEXT, `pin` TEXT );''')
+                           TEXT, `pin` TEXT );''')
 
+c.execute('''CREATE TABLE passkey (key TEXT );''')
+passkey = raw_input("Please enter your Secret Passkey: ")
+c = conn.cursor()
+c.execute('INSERT INTO passkey VALUES(?)', (safedata.encrypt(passkey),))
+conn.commit()
